d9c6f020ceffe4bc976c8761d45110e4ebd72c0a
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2023-02-14 16:59:31 +0530

    Download Name is not sequential anymore

--- 1.py
+++ 1.py
@@ -1,11 +1,22 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from  selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium import *
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
 import time
 import random
 import string
-driver = webdriver.Chrome()
+import os
+import time
+
+options = webdriver.ChromeOptions()
+
+prefs={"download.default_directory":"C:\\Users\\SUBINOY\\OneDrive\\Documents\\Python-LAPTOP-2SS827IP\\Automation\\Tickets"}
+
+options.add_experimental_option("prefs",prefs)
+driver = webdriver.Chrome(executable_path='./chromedriver',options=options)
 
 driver.maximize_window()  
 driver.get("https://htmljstemplates.com/utilities/qr-ticket-generator") 
@@ -17,8 +28,8 @@ dd.select_by_value("event")
 
 
 #Variables
-eventName="Event"
-logoURL ="C:\\Users\\SUBINOY\\OneDrive\\Pictures\\WhatsApp Image 2022-10-15 at 23.50.12.jpg" #In C:\\ABC\\ format
+eventName="ABC Competetion"
+logoURL ="C:\\Users\\SUBINOY\\OneDrive\\Documents\\Python-LAPTOP-2SS827IP\\Automation\\logo.png"
 eventDetail ="In-Person Event"
 eventLocation = "Location: " + "JIS College of Engineering, Kalyani, Nadia"
 eventDate="March 8, 2023"
@@ -56,18 +67,50 @@ element.parentNode.removeChild(element);
 """, element)
 changeforname=driver.find_element(By.XPATH,"//*[text()='Admit 1']")
 changeforrandom =driver.find_element(By.XPATH,"//*[text()='1AD3434']")
+#downloadbutton=driver.find_element(By.XPATH,"//*[text()='Donwload Ticket']")
+downloadbutton=driver.find_element(By.CLASS_NAME,'col-md-6')
+Initial_path= "C:\\Users\\SUBINOY\\OneDrive\\Documents\\Python-LAPTOP-2SS827IP\\Automation\\Tickets"
+def tiny_file_rename(newname, folder_of_download):
+    filename = max([f for f in os.listdir(folder_of_download)], key=lambda xa :   os.path.getctime(os.path.join(folder_of_download,xa)))
+    if '.part' in filename:
+        time.sleep(1)
+        os.rename(os.path.join(folder_of_download, filename), os.path.join(folder_of_download, newname+".png"))
+    else:
+        os.rename(os.path.join(folder_of_download, filename),os.path.join(folder_of_download,newname+".png"))
 def qrid(nme):
     res = ''.join(random.choices(string.ascii_uppercase +
                              string.digits, k=7))
-    pasteid = nme+ res
+    pasteid = nme + res
     print(res)
     qr.clear()
     qr.send_keys(pasteid)
+    time.sleep(1)
+    qr.send_keys(Keys.ENTER)
     time.sleep(2)
     changeforname.clear()
     changeforname.send_keys(nme)
     changeforrandom.clear()
     changeforrandom.send_keys(res)
+    time.sleep(2)
+    try:
+        element = WebDriverWait(driver, 50).until(
+        EC.element_to_be_clickable(downloadbutton))
+        time.sleep(2)
+        element.click()
+        time.sleep(1)
+        tiny_file_rename(pasteid,Initial_path)
+    except:
+        time.sleep(3)
+        downloadbutton.click()
+        time.sleep(1)
+        tiny_file_rename(pasteid,Initial_path)
+        #element = WebDriverWait(driver, 20).until(
+        #EC.element_to_be_clickable(downloadbutton))
+        #time.sleep(3)
+        #element.click()
+        #time.sleep(1)
+        #tiny_file_rename(pasteid,Initial_path)
+
 o = ["Subi","AB"]
 for pm in o:
     qrid(pm)
