fe00746be073afe91ebfa818b2a45b1305d70a9f
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2023-02-14 13:49:10 +0530

    Made it nicer to look at

--- 1.py
+++ 1.py
@@ -11,18 +11,21 @@ time.sleep(6)
 driver.find_element("id","qrInput").send_keys("Hello")
 dd= Select(driver.find_element("id","qrType"))
 dd.select_by_value("event")
-#n1= driver.find_element("xpath","//*[contains(@text='National Conference | Climate Change')]")
+
+
+#Variables
 eventName="Event"
 logoURL ="C:\\Users\\SUBINOY\\OneDrive\\Pictures\\WhatsApp Image 2022-10-15 at 23.50.12.jpg" #In C:\\ABC\\ format
 eventDetail ="In-Person Event"
 eventLocation = "Location: " + "JIS College of Engineering, Kalyani, Nadia"
 eventDate="March 8, 2023"
+
+
+#Setup
 name=driver.find_elements(By.CLASS_NAME, "h5")
 name[0].clear()
 name[0].send_keys(eventName)
 image=driver.find_elements(By.ID,"uploadLogo")
-
-#image[0].click()
 image[0].send_keys(logoURL)
 detail = driver.find_elements(By.CLASS_NAME, "smallFont")
 detail[0].clear()
@@ -30,7 +33,6 @@ detail[0].send_keys(eventDetail)
 date1 = driver.find_element(By.XPATH,"//*[text()=' 12th Dec 2022 - 22nd Dec 2022']")
 date1.clear()
 date1.send_keys(eventDate)
-
 location1 = driver.find_element(By.XPATH,"//*[text()=' A Grand Hotel, Park Ave, Bridgeport, CT']")
 location1.clear()
 location1.send_keys(eventLocation)
